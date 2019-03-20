df35a1f6310d348df6fc764653c11db415b2531e
Victor Martins <victorfrontend@gmail.com>
2019-03-19 23:45:41 -0300

    implement BlocProviderList

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:discover/pages/controllers/image_controller.dart';
+import 'package:discover/pages/controllers/rekognizer_controller.dart';
 import 'package:discover/pages/home/home.dart';
-import 'package:discover/pages/home/home_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -9,9 +10,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider<HomeController>(
+      home: BlocProviderList(
         child: Home(),
-        bloc: HomeController(),
+        listBloc: [
+          Bloc(ImageController()),
+          Bloc(RekognizerController())
+        ],
       ),
     );
   }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,7 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:discover/pages/home/home_controller.dart';
+import 'package:discover/pages/controllers/image_controller.dart';
+import 'package:discover/pages/controllers/rekognizer_controller.dart';
 import 'package:flutter/material.dart';
-// import 'package:googleapis/vision/v1.dart' hide Image, ImageSource;
 
 class Home extends StatefulWidget {
   @override
@@ -11,27 +11,43 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    final HomeController _homeController =
-        BlocProvider.of<HomeController>(context);
+    final ImageController _imageController =
+        BlocProviderList.of<ImageController>(context);
+    final RekognizerController _rekognizerController =
+        BlocProviderList.of<RekognizerController>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Discover'),
       ),
-      body: Center(
-        child: StreamBuilder(
-          stream: _homeController.outImage,
-          initialData: null,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return snapshot.hasData
-                ? Image.file(snapshot.data)
-                : Text('No image selected.');
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          StreamBuilder(
+            stream: _imageController.outImage,
+            initialData: null,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return snapshot.hasData
+                  ? Image.file(snapshot.data)
+                  : Center(child: Text('No image selected.'));
+            },
+          ),
+          StreamBuilder(
+            stream: _rekognizerController.outEntities,
+            initialData: null,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return snapshot.hasData
+                  ? Text('hasData')
+                  : RaisedButton(
+                      child: Text('Load entities'),
+                      onPressed: () => _rekognizerController.entities,
+                    );
+            },
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _homeController.bestGuessLabel;
-        },
+        onPressed: () => _imageController.pickImage,
         child: Icon(Icons.camera_alt),
       ),
     );
